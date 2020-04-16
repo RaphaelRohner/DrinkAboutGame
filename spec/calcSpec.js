13c620437a5aac3d86f2d4b443119b9a1bdf9558
Raphael Rohner <raphaelrohner@web.de>
2020-04-16 20:06:23 +0000

    two failing tests

---
+++ spec/calcSpec.js
@@ -0,0 +1,10 @@
+describe("DrinkAbout", function() {
+    describe("whatCanIDrink", function() {
+        it("should be number", function() {
+            expect(age()).toBe("Number");
+        });
+        it("return", function() {
+            expect(answer()).toBe("String");
+        });
+    });
+});
